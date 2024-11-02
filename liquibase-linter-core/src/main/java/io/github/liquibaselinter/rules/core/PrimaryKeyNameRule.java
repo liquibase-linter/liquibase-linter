1333e5387059914ce9855d7578cca4773ecef3f6
Aurélien Mino <aurelien.mino@gmail.com>
2024-11-02 23:34:05 +0100

    feat(primary-key-name): extends the rule to also apply to CreateTableChange

--- liquibase-linter-core/src/main/java/io/github/liquibaselinter/rules/core/PrimaryKeyNameRule.java
+++ liquibase-linter-core/src/main/java/io/github/liquibaselinter/rules/core/PrimaryKeyNameRule.java
@@ -3,11 +3,20 @@ package io.github.liquibaselinter.rules.core;
 import com.google.auto.service.AutoService;
 import io.github.liquibaselinter.rules.AbstractLintRule;
 import io.github.liquibaselinter.rules.ChangeRule;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import liquibase.change.Change;
+import liquibase.change.ColumnConfig;
+import liquibase.change.ConstraintsConfig;
 import liquibase.change.core.AddPrimaryKeyChange;
+import liquibase.change.core.CreateTableChange;
 
 @SuppressWarnings("rawtypes")
 @AutoService({ChangeRule.class})
-public class PrimaryKeyNameRule extends AbstractLintRule implements ChangeRule<AddPrimaryKeyChange> {
+public class PrimaryKeyNameRule extends AbstractLintRule implements ChangeRule<Change> {
     private static final String NAME = "primary-key-name";
     private static final String MESSAGE = "Primary key name is missing or does not follow pattern";
 
@@ -16,18 +25,53 @@ public class PrimaryKeyNameRule extends AbstractLintRule implements ChangeRule<A
     }
 
     @Override
-    public Class<AddPrimaryKeyChange> getChangeType() {
-        return AddPrimaryKeyChange.class;
+    public Class<Change> getChangeType() {
+        return Change.class;
     }
 
     @Override
-    public boolean invalid(AddPrimaryKeyChange change) {
-        final String constraintName = change.getConstraintName();
-        return checkMandatoryPattern(constraintName, change);
+    public boolean supports(Change change) {
+        return change.getClass().isAssignableFrom(AddPrimaryKeyChange.class)
+            || change.getClass().isAssignableFrom(CreateTableChange.class) && createTableContainsPrimaryKey((CreateTableChange)change);
+    }
+
+    private boolean createTableContainsPrimaryKey(CreateTableChange change) {
+        return primaryKeyNamesFromCreateTable(change).anyMatch(Objects::nonNull);
+    }
+
+    private static Stream<String> primaryKeyNamesFromCreateTable(CreateTableChange change) {
+        if (change.getColumns() == null) {
+            return Stream.empty();
+        }
+        return change.getColumns()
+            .stream()
+            .map(ColumnConfig::getConstraints)
+            .filter(Objects::nonNull)
+            .map(ConstraintsConfig::getPrimaryKeyName);
     }
 
     @Override
-    public String getMessage(AddPrimaryKeyChange change) {
-        return formatMessage(change.getConstraintName(), ruleConfig.getPatternString());
+    public boolean invalid(Change change) {
+        return extractConstraintNames(change).stream().anyMatch(constraintName -> checkMandatoryPattern(constraintName, change));
+    }
+
+    private Collection<String> extractConstraintNames(Change change) {
+        if (change instanceof AddPrimaryKeyChange) {
+            return Collections.singleton(((AddPrimaryKeyChange) change).getConstraintName());
+        }
+        if (change instanceof CreateTableChange) {
+            CreateTableChange createTableChange = (CreateTableChange) change;
+            return primaryKeyNamesFromCreateTable(createTableChange).collect(Collectors.toList());
+        }
+        throw new IllegalStateException("Can't retrieve constraint names from " + change.getClass());
+    }
+
+    @Override
+    public String getMessage(Change change) {
+        Collection<String> primaryKeys = extractConstraintNames(change)
+            .stream()
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+        return formatMessage(String.join(",", primaryKeys), ruleConfig.getPatternString());
     }
 }
--- liquibase-linter-core/src/test/java/io/github/liquibaselinter/rules/core/PrimaryKeyNameRuleTest.java
+++ liquibase-linter-core/src/test/java/io/github/liquibaselinter/rules/core/PrimaryKeyNameRuleTest.java
@@ -1,10 +1,13 @@
 package io.github.liquibaselinter.rules.core;
 
 import io.github.liquibaselinter.config.RuleConfig;
-import io.github.liquibaselinter.rules.core.PrimaryKeyNameRule;
+import liquibase.change.ColumnConfig;
+import liquibase.change.ConstraintsConfig;
 import liquibase.change.core.AddPrimaryKeyChange;
+import liquibase.change.core.CreateTableChange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,39 +21,90 @@ class PrimaryKeyNameRuleTest {
         primaryKeyNameRule = new PrimaryKeyNameRule();
     }
 
-    @DisplayName("Primary key name must not be null")
-    @Test
-    void primaryKeyNameMustNotBeNull() {
-        assertTrue(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange(null)));
+    @Nested
+    class AddPrimaryKey {
+
+        @DisplayName("Primary key name must not be null")
+        @Test
+        void primaryKeyNameMustNotBeNull() {
+            assertTrue(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange(null)));
+        }
+
+        @DisplayName("Primary key name must follow pattern basic")
+        @Test
+        void primaryKeyNameMustFollowPatternBasic() {
+            primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^VALID_PK$").build());
+            assertTrue(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("INVALID_PK")));
+            assertFalse(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("VALID_PK")));
+        }
+
+        @DisplayName("Primary key name must follow pattern dynamic value")
+        @Test
+        void primaryKeyNameMustFollowPatternDynamicValue() {
+            primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^{{value}}_PK$").withDynamicValue("tableName").build());
+            assertTrue(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("INVALID_PK")));
+            assertFalse(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("TABLE_PK")));
+        }
+
+        @DisplayName("Primary key name rule should support formatted error message with pattern arg")
+        @Test
+        void primaryKeyNameRuleShouldReturnFormattedErrorMessage() {
+            primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^VALID_PK$").withErrorMessage("Primary key constraints %s must follow pattern '%s'").build());
+            assertEquals(primaryKeyNameRule.getMessage(getAddPrimaryKeyConstraintChange("INVALID_PK")), "Primary key constraints INVALID_PK must follow pattern '^VALID_PK$'");
+        }
+
+        private AddPrimaryKeyChange getAddPrimaryKeyConstraintChange(String constraintName) {
+            AddPrimaryKeyChange addPrimaryKeyChange = new AddPrimaryKeyChange();
+            addPrimaryKeyChange.setConstraintName(constraintName);
+            addPrimaryKeyChange.setTableName("TABLE");
+            return addPrimaryKeyChange;
+        }
     }
 
-    @DisplayName("Primary key name must follow pattern basic")
-    @Test
-    void primaryKeyNameMustFollowPatternBasic() {
-        primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^VALID_PK$").build());
-        assertTrue(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("INVALID_PK")));
-        assertFalse(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("VALID_PK")));
-    }
+    @Nested
+    class CreateTable {
 
-    @DisplayName("Primary key name must follow pattern dynamic value")
-    @Test
-    void primaryKeyNameMustFollowPatternDynamicValue() {
-        primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^{{value}}_PK$").withDynamicValue("tableName").build());
-        assertTrue(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("INVALID_PK")));
-        assertFalse(primaryKeyNameRule.invalid(getAddPrimaryKeyConstraintChange("TABLE_PK")));
-    }
+        @DisplayName("Primary key name must not be null")
+        @Test
+        void primaryKeyNameMustNotBeNull() {
+            assertTrue(primaryKeyNameRule.invalid(createTableChange(null)));
+        }
 
-    @DisplayName("Primary key name rule should support formatted error message with pattern arg")
-    @Test
-    void primaryKeyNameRuleShouldReturnFormattedErrorMessage() {
-        primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^VALID_PK$").withErrorMessage("Primary key constraint '%s' must follow pattern '%s'").build());
-        assertEquals(primaryKeyNameRule.getMessage(getAddPrimaryKeyConstraintChange("INVALID_PK")), "Primary key constraint 'INVALID_PK' must follow pattern '^VALID_PK$'");
-    }
+        @DisplayName("Primary key name must follow pattern basic")
+        @Test
+        void primaryKeyNameMustFollowPatternBasic() {
+            primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^VALID_PK$").build());
+            assertTrue(primaryKeyNameRule.invalid(createTableChange("INVALID_PK")));
+            assertFalse(primaryKeyNameRule.invalid(createTableChange("VALID_PK")));
+        }
 
-    private AddPrimaryKeyChange getAddPrimaryKeyConstraintChange(String constraintName) {
-        AddPrimaryKeyChange addPrimaryKeyChange = new AddPrimaryKeyChange();
-        addPrimaryKeyChange.setConstraintName(constraintName);
-        addPrimaryKeyChange.setTableName("TABLE");
-        return addPrimaryKeyChange;
+        @DisplayName("Primary key name must follow pattern dynamic value")
+        @Test
+        void primaryKeyNameMustFollowPatternDynamicValue() {
+            primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^{{value}}_PK$").withDynamicValue("tableName").build());
+            assertTrue(primaryKeyNameRule.invalid(createTableChange("INVALID_PK")));
+            assertFalse(primaryKeyNameRule.invalid(createTableChange("TABLE_PK")));
+        }
+
+        @DisplayName("Primary key name rule should support formatted error message with pattern arg")
+        @Test
+        void primaryKeyNameRuleShouldReturnFormattedErrorMessage() {
+            primaryKeyNameRule.configure(RuleConfig.builder().withPattern("^VALID_PK$").withErrorMessage("Primary key constraints %s must follow pattern '%s'").build());
+            assertEquals(primaryKeyNameRule.getMessage(createTableChange("INVALID_PK")), "Primary key constraints INVALID_PK must follow pattern '^VALID_PK$'");
+        }
+
+        private CreateTableChange createTableChange(String primaryKeyName) {
+            ConstraintsConfig constraints = new ConstraintsConfig();
+            constraints.setPrimaryKeyName(primaryKeyName);
+            constraints.setPrimaryKey("true");
+
+            ColumnConfig column = new ColumnConfig();
+            column.setConstraints(constraints);
+
+            CreateTableChange createTableChange = new CreateTableChange();
+            createTableChange.setTableName("TABLE");
+            createTableChange.addColumn(column);
+            return createTableChange;
+        }
     }
 }
