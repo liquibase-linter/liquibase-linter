4f26dea2a48bc42611ba0f78b3de837597d6fc9e
Aurélien Mino <aurelien.mino@gmail.com>
2024-12-21 22:17:06 +0100

    test: rework ConditionHelper tests

--- liquibase-linter-core/src/test/java/io/github/liquibaselinter/rules/ConditionHelperTest.java
+++ liquibase-linter-core/src/test/java/io/github/liquibaselinter/rules/ConditionHelperTest.java
@@ -1,52 +1,68 @@
 package io.github.liquibaselinter.rules;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 import io.github.liquibaselinter.config.RuleConfig;
 import liquibase.ContextExpression;
 import liquibase.changelog.ChangeSet;
+import liquibase.changelog.DatabaseChangeLog;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 class ConditionHelperTest {
 
-    @Test
-    void shouldMatchSimpleContext() {
-        ChangeSet changeSet = Mockito.mock(ChangeSet.class);
-        when(changeSet.getContextFilter()).thenReturn(new ContextExpression("foo"));
-        RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('foo')").build();
-        assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isTrue();
+    @Nested
+    class MatchesContext {
+
+        @Test
+        void shouldMatchSimpleContext() {
+            ChangeSet changeSet = changeSetWithContext("foo");
+            RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('foo')").build();
+
+            assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isTrue();
+        }
+
+        @Test
+        void shouldNotMatchNegativeSimpleContext() {
+            ChangeSet changeSet = changeSetWithContext("!foo");
+            RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('foo')").build();
+
+            assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isFalse();
+        }
+
+        @Test
+        void shouldNotNatchSimpleContextMismatch() {
+            ChangeSet changeSet = changeSetWithContext("foo");
+            RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('bar')").build();
+
+            assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isFalse();
+        }
+
+        @Test
+        void shouldNotMatchNoContext() {
+            ChangeSet changeSet = changeSetWithNoContext();
+            RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('bar')").build();
+
+            assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isFalse();
+        }
+
+        @Test
+        void shouldMatchMultipleAndContexts() {
+            ChangeSet changeSet = changeSetWithContext("foo and bar");
+            RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('foo', 'bar')").build();
+
+            assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isTrue();
+        }
     }
 
-    @Test
-    void shouldNotMatchNegativeSimpleContext() {
-        ChangeSet changeSet = Mockito.mock(ChangeSet.class);
-        when(changeSet.getContextFilter()).thenReturn(new ContextExpression("!foo"));
-        RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('foo')").build();
-        assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isFalse();
+    private static ChangeSet changeSetWithContext(String contextExpression) {
+        ChangeSet changeSet = changeSetWithNoContext();
+        changeSet.setContextFilter(new ContextExpression(contextExpression));
+
+        return changeSet;
     }
 
-    @Test
-    void shouldNotNatchSimpleContextMismatch() {
-        ChangeSet changeSet = Mockito.mock(ChangeSet.class);
-        when(changeSet.getContextFilter()).thenReturn(new ContextExpression("foo"));
-        RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('bar')").build();
-        assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isFalse();
-    }
-
-    @Test
-    void shouldNotMatchNoContext() {
-        ChangeSet changeSet = Mockito.mock(ChangeSet.class);
-        RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('bar')").build();
-        assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isFalse();
-    }
-
-    @Test
-    void shouldMatchMultipleAndContexts() {
-        ChangeSet changeSet = Mockito.mock(ChangeSet.class);
-        when(changeSet.getContextFilter()).thenReturn(new ContextExpression("foo and bar"));
-        RuleConfig ruleConfig = RuleConfig.builder().withCondition("matchesContext('foo', 'bar')").build();
-        assertThat(ConditionHelper.evaluateCondition(ruleConfig, changeSet)).isTrue();
+    private static ChangeSet changeSetWithNoContext() {
+        return new ChangeSet(new DatabaseChangeLog());
     }
 }
