679826e0aecc3e0f4c99667f140730bed1e68915
Aurélien Mino <aurelien.mino@gmail.com>
2024-12-26 18:15:17 +0100

    refactor(rules-api): remove LintRule interface that is not really useful

--- liquibase-linter-core/src/main/java/io/github/liquibaselinter/rules/ChangeLogRule.java
+++ liquibase-linter-core/src/main/java/io/github/liquibaselinter/rules/ChangeLogRule.java
@@ -4,6 +4,8 @@ import io.github.liquibaselinter.config.RuleConfig;
 import java.util.Collection;
 import liquibase.changelog.DatabaseChangeLog;
 
-public interface ChangeLogRule extends LintRule {
+public interface ChangeLogRule {
+    String getName();
+
     Collection<RuleViolation> check(DatabaseChangeLog changeLog, RuleConfig ruleConfig);
 }
--- liquibase-linter-core/src/main/java/io/github/liquibaselinter/rules/ChangeRule.java
+++ liquibase-linter-core/src/main/java/io/github/liquibaselinter/rules/ChangeRule.java
@@ -4,6 +4,8 @@ import io.github.liquibaselinter.config.RuleConfig;
 import java.util.Collection;
 import liquibase.change.Change;
 
-public interface ChangeRule extends LintRule {
+public interface ChangeRule {
+    String getName();
+
     Collection<RuleViolation> check(Change change, RuleConfig ruleConfig);
 }
