d551ab60c25a108037110d4ad68ad39bb7a7e958
Aurélien Mino <aurelien.mino@gmail.com>
2026-03-08 11:58:37 +0100

    fix: use new required configuration for Cucumber and required junit version

--- liquibase-linter-core/src/test/java/io/github/liquibaselinter/cucumber/CucumberTest.java
+++ liquibase-linter-core/src/test/java/io/github/liquibaselinter/cucumber/CucumberTest.java
@@ -1,18 +1,20 @@
 package io.github.liquibaselinter.cucumber;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectDirectories;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
 @SuppressWarnings("java:S2187")
+@SelectDirectories("src/test/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.liquibaselinter")
 @ConfigurationParameter(
     key = PLUGIN_PROPERTY_NAME,
     value = "pretty, html:target/cucumber/cucumber.htm, junit:target/cucumber/TEST-cucumber.xml"
 )
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/features")
 public class CucumberTest {}
